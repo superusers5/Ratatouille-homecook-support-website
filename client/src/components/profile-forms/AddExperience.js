6d4dd00112b730398eb953f546cb155a486ab37a
Abhinav <b.abhinav2000@gmail.com>
2021-04-03 15:32:44 +0530

    c

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -150,7 +150,7 @@ const AddExperience = ({ addExperience, history }) => {
         />
 
         </div>
-        <input type="submit" className="btn btn-primary my-1" />
+        <input type="submit" className="btn btn-primary my-1" value= "Submit"/>
         <Link className="btn btn-light my-1" to="/dashboard">
           Go Back
         </Link>
